2e403e82c61e75daf6809127f67129bbdcc9590d
chian-chen <64936325+chian-chen@users.noreply.github.com>
2020-06-23 13:00:36 +0800

    Add files via upload

---
+++ final/myjs/mine.js
@@ -0,0 +1,727 @@
+// $(() => {
+//     $('#A').on('click', () => {
+
+//         console.log("aaaa");
+//         let $col = $('#displayarea>.col');
+//         $picture = $('<div>').attr('class', 'img-Picture img-A dragme')
+//         $col.append($picture)
+//     })
+// })
+
+
+$(() => {
+    $('#clear').on('click', () => {
+
+        $('#displayarea>.col').empty()
+    })
+})
+
+$(() => {
+    $('#A').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/A.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#B').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/B.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#C').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/C.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#D').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/D.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#E').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/E.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#F').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/F.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#G').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/G.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#H').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/H.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#I').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/I.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#J').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/J.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#K').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/K.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#L').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/L.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#M').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/M.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#N').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/N.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#O').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/O.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#P').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/P.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#Q').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/Q.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#R').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/R.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#S').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/S.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#T').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/T.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#U').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/U.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#V').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/V.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#W').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/W.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#X').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/X.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#Y').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/Y.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#Z').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', '/images_upper_resize/Z.png').attr('class', 'dragme').attr('height', '500px').attr('width', '500px')
+        a.append($picture)
+    })
+})
+
+
+
+
+
+
+
+
+
+$(() => {
+    $('#aa').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/aa.png").attr('class', 'dragme').attr('height', '200px').attr('width', '200px')
+        a.append($picture)
+    })
+})
+$(() => {
+    $('#bb').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/bb.png").attr('class', 'dragme').attr('height', '200px').attr('width', '200px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#cc').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/cc.png").attr('class', 'dragme').attr('height', '200px').attr('width', '200px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#dd').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/dd.png").attr('class', 'dragme').attr('height', '200px').attr('width', '200px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#ee').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/ee.png").attr('class', 'dragme').attr('height', '200px').attr('width', '200px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#ff').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/ff.png").attr('class', 'dragme').attr('height', '200px').attr('width', '200px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#gg').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/gg.png").attr('class', 'dragme').attr('height', '200px').attr('width', '200px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#hh').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/hh.png").attr('class', 'dragme').attr('height', '200px').attr('width', '200px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#ii').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/ii.png").attr('class', 'dragme').attr('height', '200px').attr('width', '200px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#jj').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/jj.png").attr('class', 'dragme').attr('height', '280px').attr('width', '280px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#kk').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/kk.png").attr('class', 'dragme').attr('height', '200px').attr('width', '200px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#ll').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/ll.png").attr('class', 'dragme').attr('height', '200px').attr('width', '200px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#mm').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/mm.png").attr('class', 'dragme').attr('height', '200px').attr('width', '200px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#nn').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/nn.png").attr('class', 'dragme').attr('height', '200px').attr('width', '200px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#oo').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/oo.png").attr('class', 'dragme').attr('height', '200px').attr('width', '200px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#pp').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/pp.png").attr('class', 'dragme').attr('height', '240px').attr('width', '240px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#qq').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/qq.png").attr('class', 'dragme').attr('height', '240px').attr('width', '240px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#rr').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/rr.png").attr('class', 'dragme').attr('height', '200px').attr('width', '200px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#ss').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/ss.png").attr('class', 'dragme').attr('height', '200px').attr('width', '200px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#tt').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/tt.png").attr('class', 'dragme').attr('height', '240px').attr('width', '240px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#uu').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/uu.png").attr('class', 'dragme').attr('height', '200px').attr('width', '200px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#vv').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/vv.png").attr('class', 'dragme').attr('height', '250px').attr('width', '250px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#ww').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/ww.png").attr('class', 'dragme').attr('height', '200px').attr('width', '200px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#xx').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/xx.png").attr('class', 'dragme').attr('height', '200px').attr('width', '200px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#yy').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/yy.png").attr('class', 'dragme').attr('height', '280px').attr('width', '280px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+    $('#zz').on('click', () => {
+
+
+        let a = $('#area')
+        $picture = $('<img>').attr('src', "/images_after/zz.png").attr('class', 'dragme').attr('height', '200px').attr('width', '200px')
+        a.append($picture)
+    })
+})
+
+$(() => {
+
+
+    function startDrag(e) {
+        // determine event object
+        if (!e) {
+            var e = window.event;
+        }
+        if (e.preventDefault) e.preventDefault();
+
+        // IE uses srcElement, others use target
+        targ = e.target ? e.target : e.srcElement;
+
+        if (targ.className != 'dragme') {
+            return
+        };
+        // calculate event X, Y coordinates
+        offsetX = e.clientX;
+        offsetY = e.clientY;
+
+        // assign default values for top and left properties
+        if (!targ.style.left) {
+            targ.style.left = '0px'
+        };
+        if (!targ.style.top) {
+            targ.style.top = '0px'
+        };
+
+        // calculate integer values for top and left 
+        // properties
+        coordX = parseInt(targ.style.left);
+        coordY = parseInt(targ.style.top);
+        drag = true;
+
+        // move div element
+        document.onmousemove = dragDiv;
+        return false;
+
+    }
+
+    function dragDiv(e) {
+        if (!drag) {
+            return
+        };
+        if (!e) {
+            var e = window.event
+        };
+        // var targ=e.target?e.target:e.srcElement;
+        // move div element
+        targ.style.left = coordX + e.clientX - offsetX + 'px';
+        targ.style.top = coordY + e.clientY - offsetY + 'px';
+        return false;
+    }
+
+    function stopDrag() {
+        drag = false;
+    }
+    window.onload = function () {
+        document.onmousedown = startDrag;
+        document.onmouseup = stopDrag;
+    }
+
+
+
+})
+
+
+
+
+//抄來但沒用的code
+
+// $(() => {
+
+//     var e = !1,
+//         a = $('#displayarea>.col')
+
+//     $("img-Picture").on("mousedown", "img-Picture", function (a) {
+//         "move" == $(this).css("cursor") ? (e = !0, movingTextarea = a.target, $(movingTextarea).css({
+//             "background-color": "rgba(192, 192, 0, 0.2)"
+//         }), starting = {
+//             X: a.pageX,
+//             Y: a.pageY
+//         }) : e = !1
+//     }).on("mousemove", "img-Picture", function (a) {
+//         0 == e ? a.offsetX < 5 || a.offsetX > $(this).innerWidth() - 5 || a.offsetY < 5 || a.offsetY > $(this).innerHeight() - 5 ? $(".text-editor").css({
+//             cursor: "move"
+//         }) : $("img-Picture").css({
+//             cursor: "text"
+//         }) : (delta = {
+//             X: a.pageX - starting.X,
+//             Y: a.pageY - starting.Y
+//         }, final = {
+//             X: $(movingTextarea).position().left + delta.X,
+//             Y: $(movingTextarea).position().top + delta.Y
+//         }, $(movingTextarea).css({
+//             left: final.X
+//         }), $(movingTextarea).css({
+//             top: final.Y
+//         }), starting = {
+//             X: a.pageX,
+//             Y: a.pageY
+//         })
+//     }).on("mouseup", "img-Picture", function (a) {
+//         e = !1, $(movingTextarea).css({
+//             "background-color": "rgba(192, 192, 192, 0)"
+//         })
+//     })
+// })
+
+
+
+
+// var element = $("#displayarea"); // global variable
+// var getCanvas; // global variable
+// $('document').ready(function () {
+//     html2canvas(element, {
+//         onrendered: function (canvas) {
+//             $("#previewImage").append(canvas);
+//             getCanvas = canvas;
+//         }
+//     });
+// });
+
+// $("#download").on('click', function () {
+//     var imgageData = getCanvas.toDataURL("image/png");
+//     // Now browser starts downloading it instead of just showing it
+//     var newData = imageData.replace(/^data:image\/png/, "data:application/octet-stream");
+//     $("#download").attr("download", "image.png").attr("href", newData);
+// });
+
+// $(function () {
+//     $("#download").click(function () {
+//         html2canvas($("#displayarea"), {
+//             onrendered: function (canvas) {
+//                 theCanvas = canvas;
+//                 document.body.appendChild(canvas);
+
+//                 // Convert and download as image 
+//                 // Canvas2Image.saveAsPNG(canvas);
+//                 // $("#previewImage").append(canvas);
+//                 // Clean up 
+//                 //document.body.removeChild(canvas);
+//             }
+//         });
+//     });
+// });
+
+
+
+// $(document).ready(function () {
+
+
+//有一點點問題
+
+$("#download").on('click', function () {
+    var element = $("#displayarea"); // global variable
+    // var getCanvas; // global variable
+    html2canvas(element, {
+        onrendered: function (canvas) {
+            $("#previewImage").append(canvas);
+            // getCanvas = canvas;
+        }
+    });
+});
+
+$(() => {
+    $('#clear2').on('click', () => {
+
+        $('#previewImage').empty()
+    })
+})
+
+
+
+//     // $("#download").on('click', function () {
+//     //     var imgageData = getCanvas.toDataURL("image/png");
+//     //     // Now browser starts downloading it instead of just showing it
+//     //     var newData = imgageData.replace(/^data:image\/png/, "data:application/octet-stream");
+//     //     $("#btn-Convert-Html2Image").attr("download", "your_pic_name.png").attr("href", newData);
+//     // });
+
+// });
